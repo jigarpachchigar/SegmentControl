485b81d0aa93972d74a108bb5f7bbba9e078b175
Jigar <Jigarpachchigar5561@gmail.com>
2020-11-25 19:17:14 +0530

    Initial Commit

---
+++ SegmentControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegmentControl
+//
+//  Created by Jigar Pachchigar on 25/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
